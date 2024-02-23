93a0f4cd078d1935afab15f6879fccffd1235986
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2024-02-23 15:08:20 +0000

    listen for peer connect

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ import { bootstrap } from '@libp2p/bootstrap'
 
 const addr = multiaddr('/ip4/127.0.0.1/tcp/23199');
 const bootstrapAddr = multiaddr('/ip4/64.227.125.94/tcp/23000/p2p/12D3KooWDu1DQcEXyJRwbq6spG5gbi11MbN3iSSqbc2Z85z7a8jB');
+const keyperAddr0 = multiaddr('/ip4/64.227.125.94/tcp/23001/p2p/12D3KooWFbscPyxc3rxyoEgyLbDYpbfx6s6di5wnr4cFz77q3taH');
+const keyperAddr1 = multiaddr('/ip4/64.227.125.94/tcp/23002/p2p/12D3KooWLmDDaCkXZgkWUnWZ1RxLzA1FHm4cVHLnNvCuGi4haGLu');
+const keyperAddr2 = multiaddr('/ip4/64.227.125.94/tcp/23003/p2p/12D3KooW9y8s8gy52jHXvJXNU5D2HuDmXxrs5Kp4VznbiBtRUnU5');
 
 const node = await createLibp2p({
     // libp2p nodes are started by default, pass false to override this
@@ -23,7 +26,7 @@ const node = await createLibp2p({
     },
     peerDiscovery: [
         bootstrap({
-            list: [bootstrapAddr]
+            list: [bootstrapAddr, keyperAddr0, keyperAddr1, keyperAddr2]
         })
     ]
     // connectionEncryption: [noise()],
@@ -38,11 +41,15 @@ node.addEventListener('peer:discovery', (evt) => {
     console.log('found peer: ' + evt.detail.id.toString())
 });
 
-node.services.pubsub.addEventListener('decryptionKey', (message) => {
-    console.log(`${message.detail.topic}:`, new TextDecoder().decode(message.detail.data))
+node.addEventListener('peer:connect', (evt) => {
+    console.log('connected to peer: ' + evt.detail.id.toString())
 });
 
-node.services.pubsub.subscribe('decryptionKey');
+// node.services.pubsub.addEventListener('decryptionKey', (message) => {
+//     console.log(`${message.detail.topic}:`, new TextDecoder().decode(message.detail.data))
+// });
+
+// node.services.pubsub.subscribe('decryptionKey');
 
 
 // update peer connections
