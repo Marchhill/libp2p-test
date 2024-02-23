205fd038d7907a3a6b0d47d65a5bad91f5cb78d2
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2024-02-23 16:07:56 +0000

    bootstrap options

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { identify } from '@chainsafe/libp2p-identify'
 import { multiaddr } from '@multiformats/multiaddr'
 import { bootstrap } from '@libp2p/bootstrap'
 
-const addr = multiaddr('/ip4/127.0.0.1/tcp/23199');
+const addr = multiaddr('/ip4/0.0.0.0/tcp/23199');
 const bootstrapAddr = multiaddr('/ip4/64.227.125.94/tcp/23000/p2p/12D3KooWDu1DQcEXyJRwbq6spG5gbi11MbN3iSSqbc2Z85z7a8jB');
 const keyperAddr0 = multiaddr('/ip4/64.227.125.94/tcp/23001/p2p/12D3KooWFbscPyxc3rxyoEgyLbDYpbfx6s6di5wnr4cFz77q3taH');
 const keyperAddr1 = multiaddr('/ip4/64.227.125.94/tcp/23002/p2p/12D3KooWLmDDaCkXZgkWUnWZ1RxLzA1FHm4cVHLnNvCuGi4haGLu');
@@ -26,7 +26,11 @@ const node = await createLibp2p({
     },
     peerDiscovery: [
         bootstrap({
-            list: [bootstrapAddr, keyperAddr0, keyperAddr1, keyperAddr2]
+            list: [bootstrapAddr, keyperAddr0, keyperAddr1, keyperAddr2],
+            timeout: 1000, // in ms,
+            tagName: 'bootstrap',
+            tagValue: 50,
+            tagTTL: 120000 // in ms
         })
     ]
     // connectionEncryption: [noise()],
@@ -35,7 +39,7 @@ const node = await createLibp2p({
 
 // start libp2p
 await node.start();
-console.log('libp2p has started');
+console.log('libp2p has started. peer id: ' + node.peerId);
 
 node.addEventListener('peer:discovery', (evt) => {
     console.log('found peer: ' + evt.detail.id.toString())
